fd04f20e50cb79781b37b69a4bae943ee462d3ec
rstmvaaz <r.aziizzzaa@gmail.com>
2024-04-12 01:55:19 +0000

    Class5

--- Class5/ec2.tf
+++ Class5/ec2.tf
@@ -1,7 +1,3 @@
-provider aws {
-    region = "us-east-2"
-}
-
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -20,30 +16,12 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-  #availability_zone = "us-east-2a"
-  #subnet_id = "subnet-0c2f142672eac68a7"
+  instance_type = var.instance_type
+  subnet_id = aws_subnet.main.id
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
-  key_name = aws_key_pair.deployer.key_name
-  count = 3
-  user_data = file ("apache.sh")
-  user_data_replace_on_change = true
-
-  tags = local.common_tags
-
+  #user_data = file("apache.sh")  
 }
 
 output ec2 {
-    value = aws_instance.web[0].public_ip
-  }
-
-output ec {
-    value = aws_instance.web[1].public_ip
-  }
-
-
-
-
-
-
-
+    value = aws_instance.web.public_ip
+}
