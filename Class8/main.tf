465f5cb35c131bb5a0d92cf76447b888c7b5f19b
rstmvaaz <r.aziizzzaa@gmail.com>
2024-04-19 01:53:38 +0000

    new files

--- Class8/main.tf
+++ Class8/main.tf
@@ -4,7 +4,7 @@ provider aws {
 
 module "vpc" {
     source = "rstmvaaz/vpc/aws"
-    version = "0.0.3"
+    version = "0.0.4"
     region = "us-east-2"
     vpc_cidr = "10.0.0.0/16"
     subnet1_cidr = "10.0.1.0/24"
